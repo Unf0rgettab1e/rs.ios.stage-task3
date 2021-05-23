c0aa5893f6bfb08e6f89dbe2af5cbfb4bbd8f6c5
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2021-05-23 16:01:29 +0300

    feat: implement PlayersSeparator method in Ex2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,20 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger countTeams = 0, countRatArr = [ratingArray count];
+    
+    for(NSInteger i = 0; i < countRatArr - 2; i++){
+        for (NSInteger j = i + 1; j < countRatArr - 1; j++) {
+            for (NSInteger k = j + 1; k < countRatArr; k++) {
+                NSInteger ratI = [ratingArray[i] integerValue],
+                ratJ = [ratingArray[j] integerValue],
+                ratK = [ratingArray[k] integerValue];
+                if(((ratI > ratJ) && (ratJ > ratK)) || ((ratI < ratJ) && (ratJ < ratK)))
+                    countTeams++;
+            }
+        }
+    }
+    return countTeams;
 }
 
 @end
